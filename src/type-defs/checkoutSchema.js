c31cebce9fe59dd6f750753d3a254933aad151de
Abduullaahh <hafizabdullahtahir@gmail.com>
2024-09-11 20:58:36 +0500

    payment

--- src/type-defs/checkoutSchema.js
+++ src/type-defs/checkoutSchema.js
@@ -1,29 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const checkoutTypeDefs = gql`
-    type Checkout {
-        success: Boolean!
-        message: String!
-        orderId: ID
-    }
+  type Checkout {
+    success: Boolean!
+    message: String!
+    orderId: ID
+    transactionId: String
+  }
 
-    input CartItem {
-        productId: ID!
-        name: String!
-        price: Float!
-        quantity: Int!
-        subtotal: Float!
-    }
+  input CartItem {
+    productId: ID!
+    name: String!
+    price: Float!
+    quantity: Int!
+    subtotal: Float!
+  }
 
-    input CheckoutInput {
-        userId: ID!
-        cart: [CartItem!]!
-        grandTotal: Float!
-    }
+  input CheckoutInput {
+    userId: ID!
+    cart: [CartItem!]!
+    grandTotal: Float!
+    paymentToken: String!
+  }
 
-    type Mutation {
-        checkout(input: CheckoutInput!): Checkout!
-    }
+  type CheckoutResult {
+    success: Boolean!
+    message: String!
+    orderId: ID
+    transactionId: String
+  }
+
+  type Mutation {
+    checkout(input: CheckoutInput!): CheckoutResult!
+  }
 `;
 
 module.exports = checkoutTypeDefs;
